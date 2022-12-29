08e234f4f4671419488a98dadac20c311c3c7127
eden lane <edencore@gmail.com>
2022-12-29 15:58:55 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://edencore.dev',
-	base: '/personal-site',
 	integrations: [mdx(), sitemap()],
 });
