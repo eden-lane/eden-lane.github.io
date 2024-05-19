07989d3ccf0671047ac4c47830c49f0a86e8df24
eden lane <edencore@gmail.com>
2024-05-19 13:06:20 +0200

    fix dynamic

--- src/app/lastfm/route.ts
+++ src/app/lastfm/route.ts
@@ -2,6 +2,8 @@ import { LastFMUser } from "lastfm-ts-api";
 
 const user = new LastFMUser(process.env.LASTFM_API_KEY!);
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const { recenttracks } = await user.getRecentTracks({ user: "YourRain", limit: 1 });
 
